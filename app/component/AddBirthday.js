f173de75ccb74dffcac6140f12b74b9223e528cc
Fortunechinenyem <fortunechinenyem@gmail.com>
2025-03-11 15:33:53 +0100

    update

--- app/component/AddBirthday.js
+++ app/component/AddBirthday.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import { collection, addDoc } from "firebase/firestore";
-import { db } from "@/lib/firebase";
+import { auth, db } from "@/lib/firebase";
 
 export default function AddBirthday() {
   const [name, setName] = useState("");
